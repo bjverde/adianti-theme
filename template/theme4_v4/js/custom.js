12aa1d271e2cf0e19fe42221f0bfbf8993aa51cb
Bjverde <bjverde@yahoo.com.br>
2023-05-31 01:35:22 -0300

    :hammer: #17 update arquivos

--- template/theme4_v4/js/custom.js
+++ template/theme4_v4/js/custom.js
@@ -1,37 +1,39 @@
-$(function () {
-    $.AdminBSB.browser.activate();
-    $.AdminBSB.leftSideBar.activate();
-    $.AdminBSB.rightSideBar.activate();
-    $.AdminBSB.navbar.activate();
-    $.AdminBSB.dropdownMenu.activate();
-    $.AdminBSB.input.activate();
-    $.AdminBSB.select.activate();
-    $.AdminBSB.search.activate();
-
-    __adianti_block_ui = function (wait_message) {
-        if (typeof wait_message !== 'undefined') {
-            $('#page-loader-message').html( wait_message );
-        }
-        $('.page-loader-wrapper').show();
-    };
-    __adianti_unblock_ui = function () { $('.page-loader-wrapper').fadeOut(); };
-    setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
+if (window.location === window.parent.location) {
+    $(function () {
+        $.AdminBSB.browser.activate();
+        $.AdminBSB.leftSideBar.activate();
+        $.AdminBSB.rightSideBar.activate();
+        $.AdminBSB.navbar.activate();
+        $.AdminBSB.dropdownMenu.activate();
+        $.AdminBSB.input.activate();
+        $.AdminBSB.select.activate();
+        $.AdminBSB.search.activate();
     
-    setTimeout( function() {
-        $('#envelope_messages a').click(function() { $(this).closest('.dropdown.open').removeClass('open'); });
-        $('#envelope_notifications a').click(function() { $(this).closest('.dropdown.open').removeClass('open'); });
-    }, 500);
-    
-    $('.menu i.fa, .menu i.fas, .menu i.far, .menu i.fab').css('zoom', '120%');
-    $('.menu i.fa, .menu i.fas, .menu i.far, .menu i.fab').css('margin-top', '8px');
-    $('.menu ul li ul li i.fa, .menu ul li ul li i.far, .menu ul li ul li i.fas, .menu ul li ul li i.fab').css('margin-top', '5px');
-    
-    $('#leftsidebar a[generator="adianti"]').click(function() {
-        $('body').scrollTop(0);
-        $('body').removeClass('overlay-open');
-        $('.overlay').hide();
+        __adianti_block_ui = function (wait_message) {
+            if (typeof wait_message !== 'undefined') {
+                $('#page-loader-message').html( wait_message );
+            }
+            $('.page-loader-wrapper').show();
+        };
+        __adianti_unblock_ui = function () { $('.page-loader-wrapper').fadeOut(); };
+        setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
+        
+        setTimeout( function() {
+            $('#envelope_messages a').click(function() { $(this).closest('.dropdown.open').removeClass('open'); });
+            $('#envelope_notifications a').click(function() { $(this).closest('.dropdown.open').removeClass('open'); });
+        }, 500);
+        
+        $('.menu i.fa, .menu i.fas, .menu i.far, .menu i.fab').css('zoom', '120%');
+        $('.menu i.fa, .menu i.fas, .menu i.far, .menu i.fab').css('margin-top', '8px');
+        $('.menu ul li ul li i.fa, .menu ul li ul li i.far, .menu ul li ul li i.fas, .menu ul li ul li i.fab').css('margin-top', '5px');
+        
+        $('#leftsidebar a[generator="adianti"]').click(function() {
+            $('body').scrollTop(0);
+            $('body').removeClass('overlay-open');
+            $('.overlay').hide();
+        });
     });
-});
+}
 
 /**
  * Show message info dialog
@@ -186,7 +188,49 @@ function tdatetime_start( id, mask, language, size, options) {
     }
 }
 
+function tdate_set_value(id, value)
+{
+    $(id).val(value);
+}
+
 $( document ).on( 'click', 'ul.dropdown-menu a[generator="adianti"]', function() {
     $(this).parents(".dropdown.show").removeClass("show");
     $(this).parents(".dropdown-menu.show").removeClass("show");
 });
+
+
+//==========================================================================================================================
+
+/* Input - Function ========================================================================================================
+*  You can manage the inputs(also textareas) with name of class 'form-control'
+*  
+*/
+$.AdminBSB.input = {
+    activate: function () {
+        //On focus event
+        $('.form-control').focus(function () {
+            // Exclude date picker
+            if ( ! $(this).parent().hasClass('tdate-group') ) {
+                // add on .formline
+                $(this).parents('.form-line').addClass('focused');
+            }
+        });
+
+        //On focusout event
+        $('.form-control').focusout(function () {
+            var $this = $(this);
+            if ($this.parents('.form-group').hasClass('form-float')) {
+                if ($this.val() == '') { $this.parents('.form-line').removeClass('focused'); }
+            }
+            else {
+                $this.parents('.form-line').removeClass('focused');
+            }
+        });
+
+        //On label click
+        $('body').on('click', '.form-float .form-line .form-label', function () {
+            $(this).parent().find('input').focus();
+        });
+
+    }
+}
